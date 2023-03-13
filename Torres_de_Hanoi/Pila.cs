dd0b83e079a4460a09a19c94cb6ba8dbb7bce3d7
OskABC <oscarcamarenap@gmail.com>
2023-03-13 15:17:26 +0100

    implementada clase pila

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Torres_de_Hanoi
-{
-    class Pila
-    {
-        public int Size { get; set; }
+namespace Torres_de_Hanoi{
+    class Pila{
         /* TODO: Elegir tipo de Top
         public int Top { get; set; }
         public String Top { get; set; }        
@@ -18,26 +16,52 @@ namespace Torres_de_Hanoi
         public List<Disco> Elementos { get; set; }
         */
 
+        public int Top { get; set; }
+        public List<Disco> Elementos { get; set; }
+        public int id { get; }
+        public int tam;
+
+
         /* TODO: Implementar métodos */
+        /*
         public Pila()
         {
 
         }
+        */
 
-        public void push(Disco d)
-        {
+        public Pila(int n){
+            this.id=n;
+            this.tam=0;
+            this.Top=null;
+            this.Elementos=new List<Disco>();
+        } // Pila()
 
-        }
+        public void push(Disco d){
+            this.Size++;
+            this.Elementos.Add(d);
+            this.Top=d;
+        } // push()
 
-        public Disco pop()
-        {
-            return null;
-        }                
+        public Disco pop(){
+            if(this.Elementos.Count==0){
+                this.Size--;
+            } // if
+            else{
+                Disco ultimo = this.Elementos[this.Elementos.Count-1];
+                this.Size--;
+                return ultimo;
+            } // else
+        } // pop()   
 
-        public bool isEmpty()
-        {
-            return true;
-        }
+        public bool isEmpty({
+            if (this.Elementos.Count == 0){
+                return true;
+            } // if
+            else{
+                return false;
+            } // else
+        } // isEmpty()
 
-    }
-}
+    } // Pila
+} // ()
