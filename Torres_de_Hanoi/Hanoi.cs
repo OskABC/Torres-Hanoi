464060acfd0f0def346187de796520cfc7f1d4e8
OskABC <oscarcamarenap@gmail.com>
2023-03-13 16:25:24 +0100

    Mover_disco implementado

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -1,23 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Torres_de_Hanoi
-{
-    class Hanoi
-    {
+namespace Torres_de_Hanoi{
+    class Hanoi{
         /*TODO: Implementar métodos*/
-        public void mover_disco(Pila a, Pila b)
-        {
+        public void mover_disco(Pila a, Pila b){
+            if(!a.isEmpty() && b.isEmpty()){
+                Disco auxiliar = a.pop();
+                b.push(auxiliar);
+                Console.WriteLine("El disco " + auxiliar.Valor + " se movió de " + a.id + " a " + b.id + ".");
+                Console.WriteLine(a.tam);
+                Console.WriteLine(b.tam);
+            } // if
+            else if (a.Top.Valor < b.Top.Valor){
+                Disco auxiliar=a.pop();
+                b.push(auxiliar);
+                Console.WriteLine("El disco " + auxiliar.Valor + " se movió de " + a.id + " a " + b.id + ".");
+                Console.WriteLine(a.tam);
+                Console.WriteLine(b.tam);
+            } // else if
+            else{
+                Console.WriteLine("No se pueden mover mas elementos de la pila"+a.id+" a la pila "+b.id+".");
+                Console.WriteLine(a.tam);
+                Console.WriteLine(b.tam);
+            } // else
 
-        }
+        } // mover_disco
 
-        public int iterativo(int n, Pila ini, Pila fin, Pila aux)
-        {
-            return 0;
-        }
-
-    }
-}
+        public int iterativo(int n, Pila ini, Pila fin, Pila aux){
+            
+        } // Iterativo
+    } // Hanoi
+} // ()
